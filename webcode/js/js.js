575cb5909f8443117a8c34916c44e7f40ffad3b7
dubing <dubing0624@gmail.com>
2017-09-04 09:39:39 +0800

    add

---
+++ webcode/js/js.js
@@ -0,0 +1,7 @@
+$(function() {
+    //第一步肯定加载configfigure文件。
+    //第二步加载分析产生场景，记载DIV
+    //$.getJSON("http://7xt8mz.com2.z0.glb.clouddn.com/ScaraconfigNew.json", Load_init) //加载jsonconfig文件
+    //分析文件产生GUI界面 
+
+})
---
+++ webcode/js/threeConfig.js
@@ -0,0 +1,76 @@
+var renderer; //声明全局变量（对象）
+//设置相机 初始化相机对象
+var camera;
+//设置场景 初始化场景，Object 
+var scene;
+//初始化render对象
+  var line;
+function initThree() {
+  width = document.getElementById('canvas3d').clientWidth; //获取画布「canvas3d」的宽
+  height = document.getElementById('canvas3d').clientHeight; //获取画布「canvas3d」的高
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  }); //生成渲染器对象（属性：抗锯齿效果为设置有效）
+  renderer.setSize(width, height); //指定渲染器的高宽（和画布框大小一致）
+  document.getElementById('canvas3d').appendChild(renderer.domElement); //追加 【canvas】 元素到 【canvas3d】 元素中。
+  // renderer.setClearColorHex(0xFFFFFF, 1.0);//设置canvas背景色(clearColor)
+  renderer.setClearColor(0xFFFFFF);
+  // renderer.alpha=true;
+}
+
+function initCamera() {
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 5000); //设置透视投影的相机,默认情况下相机的上方向为Y轴，右方向为X轴，沿着Z轴朝里（视野角：fov 纵横比：aspect 相机离视体积最近的距离：near 相机离视体积最远的距离：far）
+  camera.position.set(3.73104727684499, 38.2038025976502, 85.65486007198572);
+  camera.rotation.set( -0.419, 0.039, 0.017);
+}
+
+function initScene() {
+  scene = new THREE.Scene();
+  var grid = new THREE.GridHelper(3*robot.axis.xmax, robot.axis.xmax/5);
+  grid.rotation.set(1.58, 0, 0);
+  scene.add(grid);
+  //
+  var material = new THREE.LineBasicMaterial({
+    color: 0x0000ff
+  });
+
+  var geometry = new THREE.Geometry();
+
+  geometry.vertices.push(
+    new THREE.Vector3(0, 0, 0)
+  );
+
+  line = new THREE.Line(geometry, material);
+   
+  scene.add(line);
+
+  //对场景进行大小变化以求得适应的大小
+  // scene.scale.set(0.055, 0.055, 0.055*7/5);
+  scene.scale.set(27.5/robot.axis.xmax, 27.5/robot.axis.ymax, 27.5/robot.axis.zmax);
+  scene.position.set(0, 0, 0);
+  scene.rotation.set(-1.58, 0, 0);
+}
+//渲染对象
+function rendererFun() {
+  renderer.setPixelRatio(window.devicePixelRatio);
+   renderer.clear();
+  renderer.render(scene, camera);
+}
+//总执行对象
+function threeStart() {
+  initThree();
+  initCamera();
+  initScene();
+  // line.geometry.vertices.push(
+  //   new THREE.Vector3(1000, 0, 1000)
+  // );
+  rendererFun();
+  // line.geometry.vertices.push(
+  //   new THREE.Vector3(0, 0, 1000)
+  // );
+ 
+
+
+
+  rendererFun();
+}
